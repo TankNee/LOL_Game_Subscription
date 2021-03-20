5e264175452ac85e98bd900d79be271666d3f618
tanknee <nee@tanknee.cn>
2021-03-20 10:15:08 +0800

    add alarm ics file generation

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const API_URL = "https://lpl.qq.com/web201612/data/LOL_MATCH2_MATCH_HOMEPAGE_BMA
  *
  * @param {{}[]} games
  */
-function packageGames(games, calName) {
+function packageGames(games, hasAlarm, calName) {
     return games.map((game) => {
-        // const [fullDate, year, month, day, hour, minute] = /(\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2}):(\d{2})/g.exec(game.MatchDate);
         const gameDate = new Date(game.MatchDate);
-        gameDate.setHours(gameDate.getHours() - 8);
+        // const alarmDate = new Date(gameDate.getTime() - 30 * 60 * 1000);
         let gameName = game.bMatchName;
         const hasResult = parseInt(game.ScoreA) || parseInt(game.ScoreB);
         if (hasResult) {
@@ -21,8 +20,6 @@ function packageGames(games, calName) {
             description: `${game.GameName}${game.GameTypeName}${game.GameProcName}`,
             start: [gameDate.getFullYear(), gameDate.getMonth() + 1, gameDate.getDate(), gameDate.getHours(), gameDate.getMinutes()],
             end: [gameDate.getFullYear(), gameDate.getMonth() + 1, gameDate.getDate(), gameDate.getHours() + 2, gameDate.getMinutes()],
-            // start: [parseInt(year), parseInt(month), parseInt(day), parseInt(hour), parseInt(minute)],
-            // end: [parseInt(year), parseInt(month), parseInt(day), parseInt(hour) + 2, parseInt(minute)],
             organizer: {
                 name: `英雄联盟${game.GameName}`,
                 email: "lpl@qq.com",
@@ -31,6 +28,11 @@ function packageGames(games, calName) {
             status: "TENTATIVE",
             calName: calName ? calName : `英雄联盟${game.GameName}`,
             geo: { lat: 30.0095, lon: 120.2669 },
+            startInputType: "local",
+            startOutputType: "local",
+            endOutputType: "local",
+            endOutputType: "local",
+            alarms: hasAlarm && !hasResult ? [{ action: "audio", trigger: { minutes: 30, before: true, repeat: 1, attachType: "VALUE=URI", attach: "Glass" } }] : null,
         };
     });
 }
@@ -54,54 +56,63 @@ function getTeams(games) {
     });
     return result;
 }
-
-async function main() {
-    const buffer = (await request.get(API_URL)).body;
-    const gameBundle = JSON.parse(buffer);
+/**
+ *
+ * @param {{}} gameBundle
+ * @param {boolean} hasAlarm
+ */
+function generateICS(gameBundle, hasAlarm) {
     const ldlGameInfo = gameBundle.msg.filter((game) => game.GameName.includes("发展联赛"));
     const lplGameInfo = gameBundle.msg.filter((game) => game.GameName.includes("职业联赛"));
 
     const ldlTeams = getTeams(ldlGameInfo);
     const lplTeams = getTeams(lplGameInfo);
 
-    const ldlGames = packageGames(ldlGameInfo);
-    const lplGames = packageGames(lplGameInfo);
+    const ldlGames = packageGames(ldlGameInfo, hasAlarm);
+    const lplGames = packageGames(lplGameInfo, hasAlarm);
 
     const ldlResult = icsTool.createEvents(ldlGames);
     if (ldlResult.error) {
         console.error(ldlResult.error);
     } else {
-        fs.writeFileSync("./ldl/ldl.ics", ldlResult.value);
+        fs.writeFileSync(`./ldl/ldl${hasAlarm ? "-alarm" : ""}.ics`, ldlResult.value);
     }
     const lplResult = icsTool.createEvents(lplGames);
     if (lplResult.error) {
         console.error(lplResult.error);
     } else {
-        fs.writeFileSync("./lpl/lpl.ics", lplResult.value);
+        fs.writeFileSync(`./lpl/lpl${hasAlarm ? "-alarm" : ""}.ics`, lplResult.value);
     }
     // Team Game
     for (const key in ldlTeams) {
         if (Object.hasOwnProperty.call(ldlTeams, key)) {
             const team = ldlTeams[key];
-            const teamResult = icsTool.createEvents(packageGames(team, `${key}赛程`));
+            const teamResult = icsTool.createEvents(packageGames(team, hasAlarm, `${key}赛程`));
             if (teamResult.error) {
                 console.error(teamResult.error);
             } else {
-                fs.writeFileSync(`./ldl/team/${key}.ics`, teamResult.value);
+                fs.writeFileSync(`./ldl/team/${key}${hasAlarm ? "-alarm" : ""}.ics`, teamResult.value);
             }
         }
     }
     for (const key in lplTeams) {
         if (Object.hasOwnProperty.call(lplTeams, key)) {
             const team = lplTeams[key];
-            const teamResult = icsTool.createEvents(packageGames(team, `${key}赛程`));
+            const teamResult = icsTool.createEvents(packageGames(team, hasAlarm, `${key}赛程`));
             if (teamResult.error) {
                 console.error(teamResult.error);
             } else {
-                fs.writeFileSync(`./lpl/team/${key}.ics`, teamResult.value);
+                fs.writeFileSync(`./lpl/team/${key}${hasAlarm ? "-alarm" : ""}.ics`, teamResult.value);
             }
         }
     }
+}
+
+async function main() {
+    const buffer = (await request.get(API_URL)).body;
+    const gameBundle = JSON.parse(buffer);
+    generateICS(gameBundle, false);
+    generateICS(gameBundle, true);
 }
 
 main();
